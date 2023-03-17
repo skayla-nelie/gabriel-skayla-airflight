d4d72f78a74e93074919bcdd125317ed0b392a14
skayla <90851580+skayla-nelie@users.noreply.github.com>
2023-03-17 00:32:33 -0400

    final

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".swiper-filters", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 4,
+  spaceBetween: 10,
+  speed: 600,
+});
+
+const swipers = new Swiper(".swiper-featured-artworks", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 2,
+  spaceBetween: -800,
+  speed: 600,
+});
